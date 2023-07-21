b27204434c42d2a1bc87c2496073f7b02f0f5a8e
HOÀNG TẠ <tathanhhoang.work@gmail.com>
2023-07-22 00:07:39 +0700

    commit

--- QuanLyKho/QuanLyKho/ViewModel/AcocuntsViewModel.cs
+++ QuanLyKho/QuanLyKho/ViewModel/AcocuntsViewModel.cs
@@ -16,11 +16,70 @@ namespace QuanLyKho.ViewModel
         public ObservableCollection<Accounts> ListAccounts { get => _ListAccounts; set { _ListAccounts = value; OnPropertyChanged(); } }
         private ObservableCollection<RoleAccounts> _ListRoleAccounts;
         public ObservableCollection<RoleAccounts> ListRoleAccounts { get => _ListRoleAccounts; set { _ListRoleAccounts = value; OnPropertyChanged(); } }
+
+        private string _HoVaTen;
+        public string HoVaTen { get => _HoVaTen; set { _HoVaTen = value; OnPropertyChanged(); } }
+
+        private string _TaiKhoan;
+        public string TaiKhoan { get => _TaiKhoan; set { _TaiKhoan = value; OnPropertyChanged(); } }
+
+        private string _SDT;
+        public string SDT { get => _SDT; set { _SDT = value; OnPropertyChanged(); } }
+
+        private string _Email;
+        public string Email { get => _Email; set { _Email = value; OnPropertyChanged(); } }
+
+        private string _TenRoleTaiKhoan;
+        public string TenRoleTaiKhoan { get => _TenRoleTaiKhoan; set { _TenRoleTaiKhoan = value; OnPropertyChanged(); } }
+
+        private DateTime _NgayTao;
+        public DateTime NgayTao { get => _NgayTao; set { _NgayTao = value; OnPropertyChanged(); } }
+
+        private Accounts _SelectedItem;
+        public Accounts SelectedItem { 
+            get => _SelectedItem; 
+            set { _SelectedItem = value; 
+                OnPropertyChanged();
+                if (SelectedItem !=null) 
+                {
+                    HoVaTen = SelectedItem.TaiKhoan.HoVaTen;
+                    TaiKhoan = SelectedItem.TaiKhoan.TenTaiKhoan;
+                    SDT = SelectedItem.TaiKhoan.SDT;
+                    Email=SelectedItem.TaiKhoan.Email;
+                    TenRoleTaiKhoan = SelectedItem.TenRoleTaiKhoan;
+                    NgayTao =(DateTime)SelectedItem.TaiKhoan.NgayTao;
+
+                }
+            } 
+        }
+
+        private string _TenRoleTaiKhoan2;
+        public string TenRoleTaiKhoan2 { get => _TenRoleTaiKhoan2; set { _TenRoleTaiKhoan2 = value; OnPropertyChanged(); } }
+        private RoleAccounts _SelectedItemRole;
+        public RoleAccounts SelectedItemRole
+        {
+            get => _SelectedItemRole;
+            set
+            {
+                _SelectedItemRole = value;
+                OnPropertyChanged();
+                if (SelectedItemRole != null)
+                {
+                    RoleAccountViewModel rlVM = new RoleAccountViewModel();
+                    TenRoleTaiKhoan2 = SelectedItemRole.RoleTK.Ten;
+
+                }
+            }
+        }
         public AcocuntsViewModel()
         {
+          
             LoadAccount();
             RoleAccountViewModel role = new RoleAccountViewModel();
             ListRoleAccounts=role.LoadRole();
+            //ListSelectRoleAccounts = ListRoleAccounts[0].RoleTK.Ten;
+
+
         }
         public void LoadAccount()
         {
--- QuanLyKho/QuanLyKho/ViewModel/CustomerViewModel.cs
+++ QuanLyKho/QuanLyKho/ViewModel/CustomerViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace QuanLyKho.ViewModel
 {
@@ -12,6 +13,30 @@ namespace QuanLyKho.ViewModel
     {
         private ObservableCollection<Customers> _ListCustomers;
         public ObservableCollection<Customers> ListCustomers { get => _ListCustomers; set { _ListCustomers = value; OnPropertyChanged(); } }
+
+        private string  _TenKhachHang;
+        public string TenKhachHang { get => _TenKhachHang; set { _TenKhachHang = value; OnPropertyChanged(); } }
+
+        private string _SDT;
+        public string  SDT { get => _SDT; set { _SDT = value; OnPropertyChanged(); } }
+
+        private string _Email;
+        public string Email { get => _Email; set { _Email = value; OnPropertyChanged(); } }
+
+        private string _ThongTinThem;
+        public string ThongTinThem { get => _ThongTinThem; set { _ThongTinThem = value; OnPropertyChanged(); } }
+
+        private Customers _SeletedItem;
+        public Customers SelectedItem { get => _SeletedItem; set { _SeletedItem = value; OnPropertyChanged(); 
+                if (SelectedItem !=null) 
+                {
+                    TenKhachHang = SelectedItem.KhachHang.Ten;
+                    SDT = SelectedItem.KhachHang.SDT;
+                    Email = SelectedItem.KhachHang.Email;
+                    ThongTinThem = SelectedItem.KhachHang.ThongTinThem;
+                } 
+            } 
+        }
         public CustomerViewModel()
         {
             LoadCustomer();
