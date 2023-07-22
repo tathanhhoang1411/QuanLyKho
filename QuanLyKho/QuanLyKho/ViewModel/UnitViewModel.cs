aa8eeb5c0d5f879b92508734f8607c182ef6f58c
HOÀNG TẠ <tathanhhoang.work@gmail.com>
2023-07-22 17:16:33 +0700

    commit

--- QuanLyKho/QuanLyKho/ViewModel/UnitViewModel.cs
+++ QuanLyKho/QuanLyKho/ViewModel/UnitViewModel.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using System.Windows.Forms.VisualStyles;
+using System.Data.Entity.Core.Objects;
 
 namespace QuanLyKho.ViewModel
 {
@@ -40,13 +42,40 @@ namespace QuanLyKho.ViewModel
         {
 
             LoadUnit();
-            AddCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            AddCommand = new RelayCommand<object>((p) => { return CanAddCommand(); }, (p) => { ExcutedAddCommand(); });
+        }
+        private void ExcutedAddCommand()
+        {
+            DonViDo unit = new DonViDo();
+            unit.Ten= TenDonViDo;
+                DataProvider.Ins.DB.DonViDoes.Add(unit);
+                DataProvider.Ins.DB.SaveChanges();
+
+            //phương án 1: bỏ unit vào vị trí cuối của ListDonViDoes vì ListDonViDoes có OnPropertyChanged()
+            //kết quả: không load lại ListDonViDoes
+            //int countItem=ListDonViDoes.Count();
+            //ListDonViDoes[countItem - 1].STT= 100;
+            //ListDonViDoes[countItem - 1].DonViDo = unit;
+            
+            //phương án 2: gọi lại hàm LoadUnit() chắc chắc sẽ load lại ListDonViDoes vì trong hàm này có ListDonViDoes
+            //Kết quả: thành công nhưng vì mất thời gian gọi nên sẽ lâu hơn với dữ liệu lớn 
+            LoadUnit();
+
+        }
+        private bool CanAddCommand()
+        {
+            if (string.IsNullOrEmpty(TenDonViDo))//Nếu TenDonViDo rỗng(nó là 1 textbox được binding từ unitVM)
             {
-                MainWindow mainVM = new MainWindow();
-                mainVM.Hide();
-                CustomerWindow wd = new CustomerWindow();
-                wd.ShowDialog();
-            });
+                return false;
+            }
+            //nếu tên đơn vị đo có kí tự thì ....
+            var list = DataProvider.Ins.DB.DonViDoes.Where(x => x.Ten == TenDonViDo);//Lấy ra List đơn vị đo có Tên == giá trị từ TenDonViDo 
+            if ( list.Count() == 0)//Nếu List đơn vị đo chưa từng tồn tại DonViDo.Ten có giá trị TenDonViDo
+            {
+                return true;
+            }
+
+            return false;
         }
         public void LoadUnit()
         {
